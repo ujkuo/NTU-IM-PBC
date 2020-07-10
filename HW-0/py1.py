583fb1a211d6d14544ce21407c4b96fad450cc32
ujkuo <s9501667@gmail.com>
2020-07-11 00:39:55 +0800

    add hw0 file

--- HW-0/py1.py
+++ HW-0/py1.py
@@ -1,11 +1,14 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#   Version 
-#   Author: ujkuo
-#   GitHub: github.com/ujkuo
-#   Copyleft (C) 2020 ujkuo All rights reversed.
-#
-
 if __name__ == "__main__":
+nswer = []
+for i in range(4):
+        num.append(int(input()))
+result = num[2] - num[1]
+for i in range(2):
+    delta = num[i*2+1] - num[i*2]
+    answer.append(delta)
+    result *= answer[i]
+answer.append(result)
+
+print(answer[0],answer[1],answer[2], sep = ',')
 
 
